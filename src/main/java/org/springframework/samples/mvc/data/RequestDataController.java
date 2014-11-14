c642009971bad1ea171a034efca98389b513c138
myyuren <myyuren@163.com>
2014-11-14 11:02:27 +0800

    首次提交

--- src/main/java/org/springframework/samples/mvc/data/RequestDataController.java
+++ src/main/java/org/springframework/samples/mvc/data/RequestDataController.java
@@ -12,13 +12,29 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.linda.framework.rpc.RpcObject;
+import com.linda.framework.rpc.nio.NioClient;
+import com.linda.framework.rpc.nio.RpcNioConnector;
+import com.linda.framework.rpc.nio.RpcNioSelection;
+
 @Controller
 @RequestMapping("/data")
 public class RequestDataController {
 
 	@RequestMapping(value="param", method=RequestMethod.GET)
 	public @ResponseBody String withParam(@RequestParam String foo) {
-		return "Obtained 'foo' query parameter value '" + foo + "'";
+		System.out.println("***********************************");
+		RpcNioSelection selection = new RpcNioSelection();
+		NioClient nioClient = new NioClient(selection);
+		String ip = "hadoop2";
+		int basePort = 8000;
+		RpcNioConnector connector = nioClient.getConnector(ip, basePort);
+		connector.startService();
+		RpcObject rpc = nioClient.createRpc("test send info",1,2);
+		
+		boolean flag = connector.sendRpcObject(rpc, 10000);
+		
+		return "Obtained 'foo' query parameter value '" + String.valueOf(flag) + "'";
 	}
 
 	@RequestMapping(value="group", method=RequestMethod.GET)
